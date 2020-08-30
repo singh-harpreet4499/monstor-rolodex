716c204c8c4c2703714576c66d14f062fcc71883
Harpreet <singh.harpreet4499@gmail.com>
2020-08-30 14:16:32 +0530

    Updates

---
+++ precache-manifest.b8aa9a461a407e38cfb015813d852712.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42c9c0fb43bccb1f4103de15bfddafcb",
+    "url": "/monstor-rolodex/index.html"
+  },
+  {
+    "revision": "82e63f5ddc3f7b238f9b",
+    "url": "/monstor-rolodex/static/css/main.d33b2cd6.chunk.css"
+  },
+  {
+    "revision": "6857abc247f93c90f1cf",
+    "url": "/monstor-rolodex/static/js/2.c0ed5171.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstor-rolodex/static/js/2.c0ed5171.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82e63f5ddc3f7b238f9b",
+    "url": "/monstor-rolodex/static/js/main.f2c1d5ed.chunk.js"
+  },
+  {
+    "revision": "9a0bafa94f123dd0b571",
+    "url": "/monstor-rolodex/static/js/runtime-main.062f9c3c.js"
+  }
+]);
